1f8bdcb957202ea37c0edea6060f0f8ad3a82850
Luca Palermo <lucaapalermo@gmail.com>
2020-04-19 16:46:15 -0700

    Add captureCharge function
    Add skeleton function for capturing charges after placing a hold on a
    user's card. Implement code for placing a hold on a card in comments.
    Update documentation.

--- functions/index.js
+++ functions/index.js
@@ -15,6 +15,7 @@ admin.initializeApp();
  *
  * @link https://firebase.google.com/docs/functions/callable
  * @link https://stripe.com/docs/payments/accept-a-payment-charges#web-create-charge
+ * @link https://stripe.com/docs/charges/placing-a-hold#authorize-a-payment
  *
  * @param {Object} param0         Object containing source and subject.
  * @param {string} param0.source  Token generated through credit card or native pay.
@@ -29,9 +30,38 @@ exports.createCharge = functions.https.onCall(
     currency: 'usd',
     description: `${subject ? `${subject} ` : ''}Tutoring Session`,
     source,
+    // capture: false, TODO
   })
-    .then(() => 'Success')
+    .then(({ id }) => {
+      // TODO Save the returned charge ID to later be used for capturing the charge.
+
+      return 'Success';
+    })
     .catch((error) => {
       throw new functions.https.HttpsError('create-charge-error', error.message, error);
+    })
+);
+
+/**
+ * Captures a charge.
+ *
+ * Intakes a charge ID generated from the createCharge function and captures the funds from it.
+ * This function should be called programmatically 24 hours after a user's session with a provider
+ * given that no disputes took place. Otherwise, the charge will be automatically released after 7
+ * days.
+ *
+ * @since 0.0.4
+ *
+ * @link https://firebase.google.com/docs/functions/callable
+ * @link https://stripe.com/docs/charges/placing-a-hold#capture-the-funds
+ *
+ * @returns {string}                     "Success" if successfully captured charge.
+ * @throws  {functions.https.HttpsError} Any error that occurs during capturing.
+ */
+exports.captureCharge = functions.https.onCall(
+  (chargeId) => stripe.charges.capture(chargeId)
+    .then(() => 'Success')
+    .catch((error) => {
+      throw new functions.https.HttpsError('capture-charge-error', error.message, error);
     })
 );
