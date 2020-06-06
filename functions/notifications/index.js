d84113d4869cf3aff484a66e0fcb5b1e01428a28
Brett <stoddabr@oregonstate.edu>
2020-06-05 20:07:57 -0700

    update captureCharge to use db data, fix admin import in reservation
    the change to captureCharge is necessary so that the transactionId can
    be handled gracefully by the API state event. This is the same pattern
    used when triggering call notifications (another function triggered by
    an API state event)

--- functions/notifications/index.js
+++ functions/notifications/index.js
@@ -156,38 +156,38 @@ const dispatchAndroid = async ({ messaging, consumerNotifId, notif }) => messagi
 exports.triggerIncomingCall = ({ slotId }) => {
   const { db, messaging } = require('../_helpers/initialize_admin');
 
-  if (slotId) {
-    return db.doc(`Schedule/${slotId}`).get()
-      .then((doc) => doc.data())
-      .then(({ property, consumerNotifId }) => {
-        let callerName = '';
-
-        const parts = property.split('_');
-        if (parts.length > 1) {
-          callerName = `${parts[0].slice(0, 1).toUpperCase()}${parts[0].slice(1)} Tutor`;
-        } else {
-          callerName = 'Tutor';
-        }
-
-        const data = {
-          isCall: true,
-          consumerNotifId,
-          notif: {
-            handle: 'WaTutors',
-            callerName,
-          },
-        };
-
-        if (consumerNotifId.includes('/3/device')) return dispatchIOS(data);
-
-        return dispatchAndroid({ messaging, ...data });
-      })
-      .catch((error) => {
-        throw new https.HttpsError('unknown', error.message, error);
-      });
+  if (!slotId) {
+    return new https.HttpsError('invalid-argument', 'Falsy slotId.');
   }
 
-  throw new https.HttpsError('invalid-argument', 'Missing slotId.');
+  return db.doc(`Schedule/${slotId}`).get()
+    .then((doc) => doc.data())
+    .then(({ property, consumerNotifId }) => {
+      let callerName = '';
+
+      const parts = property.split('_');
+      if (parts.length > 1) {
+        callerName = `${parts[0].slice(0, 1).toUpperCase()}${parts[0].slice(1)} Tutor`;
+      } else {
+        callerName = 'Tutor';
+      }
+
+      const data = {
+        isCall: true,
+        consumerNotifId,
+        notif: {
+          handle: 'WaTutors',
+          callerName,
+        },
+      };
+
+      if (consumerNotifId.includes('/3/device')) return dispatchIOS(data);
+
+      return dispatchAndroid({ messaging, ...data });
+    })
+    .catch((error) => {
+      throw new https.HttpsError('unknown', error.message, error);
+    });
 };
 
 /**
