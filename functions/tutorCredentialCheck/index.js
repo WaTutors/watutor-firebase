4b4dab43bc62d4e653d1424184e59425418c4689
Brett <stoddabr@oregonstate.edu>
2020-06-18 17:31:22 -0700

    eslint fixes

--- functions/tutorCredentialCheck/index.js
+++ functions/tutorCredentialCheck/index.js
@@ -3,53 +3,53 @@ const bucketName = 'watutors-1.appspot.com';
 // Simple pattern search through all text in document
 // TODO revise
 function searchText(text, pattern, start) {
-    const minIndex = text.indexOf(pattern, start || 0);
-    return minIndex;
-};
+  const minIndex = text.indexOf(pattern, start || 0);
+  return minIndex;
+}
 
 // SECTION
 exports.verifyCredential = async (data) => {
-    const { cert, legalName, state } = data.cred;
-    const uid = data.uid;
-    const legalNameParts = legalName.split(' ');
-    const firstName = legalNameParts[0];
-    const lastName = legalNameParts[legalNameParts.length - 1];
+  const { cert, legalName, state } = data.cred;
+  const { uid } = data;
+  const legalNameParts = legalName.split(' ');
+  const firstName = legalNameParts[0];
+  const lastName = legalNameParts[legalNameParts.length - 1];
 
-    // Get text annotation from Google Cloud Vision API
-    const { annotate } = require('../_helpers/googleCloudVision');
-    const gcsSourceUri = `gs://${bucketName}/${uid}/cert/${cert}`;
-    const gcvResult = await annotate(gcsSourceUri);
-    const text = gcvResult.fullTextAnnotation.text.toLowerCase(); // NOTE
+  // Get text annotation from Google Cloud Vision API
+  const { annotate } = require('../_helpers/googleCloudVision');
+  const gcsSourceUri = `gs://${bucketName}/${uid}/cert/${cert}`;
+  const gcvResult = await annotate(gcsSourceUri);
+  const text = gcvResult.fullTextAnnotation.text.toLowerCase(); // NOTE
 
-    let messages = new Array();
-    // Verify first name occurs in document
-    const firstNameIndex = searchText(text, firstName.toLowerCase());
-    if (firstNameIndex == -1) {
-        messages.push(`Unable to recognize first name '${firstName}' in uploaded document`);
-    }
-    // Verify last name occurs in document
-    const lastNameIndex = searchText(text, lastName.toLowerCase(), firstNameIndex);
-    if (lastNameIndex == -1) {
-        messages.push(`Unable to recognize last name '${lastName}' in uploaded document`);
-    }
-    // Verify state name occurs in document
-    const stateIndex = searchText(text, state.toLowerCase());
-    if (stateIndex == -1) {
-        messages.push(`Unable to recognize state '${state}' in uploaded document`);
-    }
+  const messages = [];
+  // Verify first name occurs in document
+  const firstNameIndex = searchText(text, firstName.toLowerCase());
+  if (firstNameIndex === -1) {
+    messages.push(`Unable to recognize first name '${firstName}' in uploaded document`);
+  }
+  // Verify last name occurs in document
+  const lastNameIndex = searchText(text, lastName.toLowerCase(), firstNameIndex);
+  if (lastNameIndex === -1) {
+    messages.push(`Unable to recognize last name '${lastName}' in uploaded document`);
+  }
+  // Verify state name occurs in document
+  const stateIndex = searchText(text, state.toLowerCase());
+  if (stateIndex === -1) {
+    messages.push(`Unable to recognize state '${state}' in uploaded document`);
+  }
 
-    // Prepare response and send notification to Support (if necessary)
-    const body = {};
-    if (messages.length == 0) {
-        body.valid = 'yes';
-    } else {
-        body.valid = 'pending';
-        body.messages = messages;
-        const { manualVerificationEmail } = require('../sendEmail');
-        const res = await manualVerificationEmail(uid, messages);
-        console.assert(res.accepted.length === 1);
-    }
-    return body;
+  // Prepare response and send notification to Support (if necessary)
+  const body = {};
+  if (messages.length === 0) {
+    body.valid = 'yes';
+  } else {
+    body.valid = 'pending';
+    body.messages = messages;
+    const { manualVerificationEmail } = require('../sendEmail');
+    const res = await manualVerificationEmail(uid, messages);
+    console.assert(res.accepted.length === 1);
+  }
+  return body;
 };
 
 // !SECTION
